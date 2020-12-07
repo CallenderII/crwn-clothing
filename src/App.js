df39e07309d423a6b9bef93f731ec8de02758fc0
Andrew Callender <andrewcallender@Andrews-MacBook-Pro-2.local>
2020-12-07 23:19:00 +0000

    Adding header component and accompanying links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={HomePage}></Route>
-          <Route path='/shop' component={ShopPage}></Route>
-        </Switch>
-      </BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
