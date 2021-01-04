524570d438c73b505390b56fec7e4fd2c035ba35
Andrew Callender <andrewcallender@Andrews-MacBook-Pro-2.local>
2021-01-04 16:16:50 -0500

    Adding sign-in functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,10 +13,28 @@ const config = {
     measurementId: "G-V9Y6WEGRFE"
 };
 
-firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = firebase.firestore.Timestamp.now();
+
+        try {
+            await userRef.set({
+                displayName, email, createdAt, ...additionalData
+            })
+        } catch (e) {
+            console.log('Error creating user', e.message);
+        }
+    }
+    return userRef;
+};
+
+firebase.initializeApp(config);
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
@@ -24,6 +42,9 @@ provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 
 
